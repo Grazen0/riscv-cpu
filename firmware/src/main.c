06456203e67af962e3ae8469a559158aff4bdd70
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-25 14:02:07 -0500

    refactor(firmware): more firmware refactor

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -6,7 +6,7 @@ void start(void)
 {
     lcd_send_instr(LCD_CLEAR);
     lcd_send_instr(LCD_RETURN_HOME);
-    lcd_send_instr(LCD_DISPLAY_CONTROL(LCD_DC_DISPLAY | LCD_DC_CURSOR | LCD_DC_BLINK));
+    lcd_send_instr(LCD_DISPLAY_CONTROL(LCDDC_DISPLAY | LCDDC_CURSOR | LCDDC_BLINK));
 
     int *arr_1 = malloc(sizeof(*arr_1) * 5);
     int *arr_2 = malloc(sizeof(*arr_2) * 8);
